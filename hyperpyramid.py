b7a7fcfe57a16856c53be17f24319d677724f33f
Ackbach <Ackbeet@gmail.com>
2018-06-14 21:48:15 -0400

    Starting in on node class definition.

---
+++ hyperpyramid.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Some important numerical libraries we'll need. Might need scipy, we'll see.
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+""" ----- Node class ----- """
+
+
+class HyperPyramidNode:
+
+    def __init__(self):
